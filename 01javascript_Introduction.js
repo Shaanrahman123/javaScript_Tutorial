548570d0680abe1a4acbba92fa2c784d05ebbf0e
shaanrahman123 <coderraufur@gmail.com>
2023-07-05 22:37:08 +0530

    lecture1

---
+++ 01javascript_Introduction.js
@@ -0,0 +1,26 @@
+// javascript was invented in 1997 by brendan eich.
+
+//   ecmascript
+// 1st edition of ecmascript was released in 1997.
+// the first main revision es5 (2009);
+// the second revision ES6 (2015);
+
+// what is javascript  ?
+
+//   javascript is very lightweight and objext oriented programming language;
+
+// javascript is scripting language;
+// extenstion of javascript .js;
+// javascript is dynamically typed language;
+
+// javascript library : React.js
+// javascript frontend framework : Angular.js, vue.js
+// javascript backend framework : Express.js
+// javascript environment : Node.js
+
+
+
+
+// to print anything we use console.log();
+// console.log("Hello world this is my first javascript program !");
+
