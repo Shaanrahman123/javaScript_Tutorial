e8353ba60ce67990fbde1b24b2fa7e45a14166b8
shaanrahman123 <coderraufur@gmail.com>
2023-07-06 22:02:10 +0530

    lecture1

--- 01javascript_Introduction.js
+++ 01javascript_Introduction.js
@@ -13,7 +13,7 @@
 // extenstion of javascript .js;
 // javascript is dynamically typed language;
 
-// javascript library : React.js
+// javascript frontend library : React.js
 // javascript frontend framework : Angular.js, vue.js
 // javascript backend framework : Express.js
 // javascript environment : Node.js
@@ -44,6 +44,7 @@
 
 
 // var is global scoped variable
+
 // var a = 5;
 // console.log(a);
 // a = "javascript"; (we can reassigned in var)
@@ -55,6 +56,49 @@
 // console.log(b);
 // let b = "javascript"; (we cannot reassigned in let )
 // console.log(b)
+
+
+
+// rules for choosing variable names
+
+// letterr, digits and underscore are allowed;
+
+// let a = 5 (it will work)
+//let $a = 5 (it will word)
+
+//let _a = 5 (it will not work);
+//let 5a = 5 (it will not work);
+//let #a = 5 (it will not work);
+
+// it must starts with letters or $
+
+// javascript reserved word cannot be used as a variable name;
+
+// let var and const
+
+// the var keyword was used in javascript from 1995 to 2015 and the let and const keyword were added in 2015;
+
+// var
+
+// var can be updated and can be redeclared
+
+// var a = 10;
+// var a = 20(it will work)
+// a = 30; (it will also work)
+
+// const
+
+//const can neither be updated nor be redeclared
+// const x = 10;
+// x = 5; (it will not work)
+// const x = 5 (it will not work)
+
+// let can be updated but not re declared
+
+// let a = 45;
+// a = 10; (it will work)
+// let a = 3; (it will not work)
+
 
 
 
